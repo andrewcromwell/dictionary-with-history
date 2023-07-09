21d0794bae157650d84c29a325fded793f2fafdf
Andrew Cromwell <andrewcromwell2012@gmail.com>
2023-07-08 23:13:42 -0500

    set up the React app

---
+++ stacks/FrontendStack.js
@@ -0,0 +1,28 @@
+import { StaticSite, use } from "sst/constructs";
+import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
+
+export function FrontendStack({ stack, app }) {
+  const { api } = use(ApiStack);
+  const { auth } = use(AuthStack);
+
+  // Define our React app
+  const site = new StaticSite(stack, "ReactSite", {
+    path: "frontend",
+    buildOutput: "build",
+    buildCommand: "npm run build",
+    // Pass in our environment variables
+    environment: {
+      REACT_APP_API_URL: api.customDomainUrl || api.url,
+      REACT_APP_REGION: app.region,
+      REACT_APP_USER_POOL_ID: auth.userPoolId,
+      REACT_APP_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId,
+      REACT_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+    },
+  });
+
+  // Show the url in the output
+  stack.addOutputs({
+    SiteUrl: site.url || "http://localhost:3000",
+  });
+}
